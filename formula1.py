7d9d0511313af7ec01cdff1d64a04b501390a9d8
Daniel Choi <CYDP0127@users.noreply.github.com>
2018-03-25 17:38:52 +0900

    소스정리

--- formula1.py
+++ formula1.py
@@ -1,6 +1,7 @@
 import math
 def starsort(ns):
     """
+    references
     http://www.evanmiller.org/ranking-items-with-star-ratings.html
     https://stackoverflow.com/questions/1411199/what-is-a-better-way-to-sort-by-a-5-star-rating
     """
@@ -15,5 +16,3 @@ def starsort(ns):
         return sum(sk*(nk+1) for sk, nk in zip(s,ns)) / (N+K)
     fsns = f(s, ns)
     return fsns - z*math.sqrt((f(s2, ns)- fsns**2)/(N+K+1))
-
-print(starsort((1,0,0,0,0,0,0,0,0,0)))
--- formula2.py
+++ formula2.py
@@ -1,22 +1,8 @@
 def starSortSimple(ns):
-    v = sum(ns)
-    m = 3 # minimum vote
-    C = 4.5867
+    v = sum(ns) # number of votes
+    m = 3 # minimum votes the be exposed on the list
+    C = 4.5867 # the mean vote across the whole report
+    # R: average ratings
     R = ((5 * ns[0]) + (4.5 * ns[1]) + (4 * ns[2]) + (3.5 * ns[3]) + (3 * ns[4]) + (2.5 * ns[5]) + (2 * ns[6]) + (1.5 * ns[7]) + (1 * ns[8]) + (0.5 * ns[9]) + 0) / v
     return ((v / (v + m)) * R) + (( m / ( v + m )) * C)
-    # return (v * R + m * C) / (v + m)
-print(starSortSimple((	1	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	)))
-print(starSortSimple((	0	,	5	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	)))
-print(starSortSimple((	0	,	0	,	10	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	)))
-print(starSortSimple((	0	,	0	,	0	,	20	,	0	,	0	,	0	,	0	,	0	,	0	,	0	)))
-print(starSortSimple((	0	,	0	,	0	,	0	,	30	,	0	,	0	,	0	,	0	,	0	,	0	)))
-print(starSortSimple((	0	,	0	,	0	,	0	,	0	,	40	,	0	,	0	,	0	,	0	,	0	)))
-print(starSortSimple((	0	,	0	,	0	,	0	,	0	,	0	,	50	,	0	,	0	,	0	,	0	)))
-print(starSortSimple((	0	,	0	,	0	,	0	,	0	,	0	,	0	,	60	,	0	,	0	,	0	)))
-print(starSortSimple((	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	70	,	0	,	0	)))
-print(starSortSimple((	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	80	,	0	)))
-print(starSortSimple((	100	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	)))
-print(starSortSimple((	60	,	20	,	10	,	5	,	3	,	1	,	1	,	0	,	0	,	0	,	0	)))
-print(starSortSimple((	50	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	0	,	50	,	0	)))
-print(starSortSimple((	30	,	15	,	5	,	0	,	0	,	0	,	0	,	5	,	15	,	30	,	0	)))
-print(starSortSimple((	10	,	10	,	10	,	10	,	10	,	10	,	10	,	10	,	10	,	10	,	10	)))
+    # return (v * R + m * C) / (v + m) it's the same
